77d41e76089b97e56a1a946d63b31b45b99d0782
schuck <zzztttztzt@126.com>
2024-04-18 12:07:20 +0800

    feat: first add

---
+++ src/main/java/com/hodoni/pandio/PandioApplication.java
@@ -0,0 +1,13 @@
+package com.hodoni.pandio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hodoni/pandio/PandioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodoni.pandio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
